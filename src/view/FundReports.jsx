e1b0b7f1d163b8e25ea88ee81ea797589f181fe3
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2022-03-05 09:56:34 -0300

    Implementando react-select

--- src/view/FundReports.jsx
+++ src/view/FundReports.jsx
@@ -4,11 +4,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from "../components/Table.jsx"
 import { useParams } from 'react-router-dom'
+import { Chart } from "react-google-charts"
+import CurrencyFormat from 'react-currency-format';
 import axios from "axios";
+
+
+const options = {
+  title: "Composicao da Carteira",
+};
+
+const today = new Date();
 
 const FundReports = () => {
   const [security, setSecurity] = React.useState([]);
+  const [fund, setFund] = React.useState([]);
+  const [yesterday, setYesterday] = React.useState([]);
+  const [dtoday, setToday] = React.useState([]);
+  const [tomorrow, setTomorrow] = React.useState([]);
+  const [pl, setPl] = React.useState([]);
   const { id } = useParams() || null;
+  const data = [["Ativos", "quantidade"]];
+  
   const columns = React.useMemo(
       () => 
       [
@@ -22,24 +38,69 @@ const FundReports = () => {
       ], []
   );
 
+
+
    React.useEffect(() => {
+      axios.get(`http://localhost:3001/funds/${id}.json`) 
+      .then((resp) => {
+        setFund(resp.data);
+      })
+        .catch((err) => {
+        console.log("Error: ", err); 
+     }); 
     axios
       .get(`http://localhost:3001/portifolios/ativos/${id}`)
         .then((resp) => { setSecurity(resp.data); })
         .catch((err) => { console.log(err); });
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ (today.getDate() - 1);   
+    axios
+      .get(`http://localhost:3001//portifolios/saldo/${id}/${date}`)
+        .then((resp) => { setYesterday(resp.data); })
+        .catch((err) => { console.log(err); });
+    date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ (today.getDate());
+    axios
+      .get(`http://localhost:3001/portifolios/pl/${id}/${date}`)
+        .then((resp) => { setPl(resp.data); })
+        .catch((err) => { console.log(err); }); 
+    axios
+      .get(`http://localhost:3001//portifolios/saldo/${id}/${date}`)
+        .then((resp) => { setToday(resp.data); })
+        .catch((err) => { console.log(err); }); 
+    date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ (today.getDate() + 1);
+    axios
+      .get(`http://localhost:3001//portifolios/saldo/${id}/${date}`)
+        .then((resp) => { setTomorrow(resp.data); })
+        .catch((err) => { console.log(err); }); 
+      
   }, []);
 
-      
+    security.map((e) => {
+      data.push([e.security.security_simbol, e.security_quantity]);
+    })
     return (
       <Container className="mt-4" fluid="sm">
         <Row>
-          <Col>PL: </Col>
-          <Col>Saldo em Caixa: </Col>
-          <Col>D-0: </Col>
-          <Col>D-1: </Col>
+         <Col><h2>{fund.name_fund}</h2></Col>
+        </Row>
+        <Row>
+        <Col><strong>Data: {today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear()}</strong></Col> 
         </Row>
         <Row className="mt-4">
-          <Col>Grafico</Col>
+          <Col>
+            <Chart
+              chartType="PieChart"
+              data={data}
+              options={options}
+              width={"100%"}
+              height={"400px"}
+            />
+          </Col>
+        </Row>
+        <Row className="mt-2"> 
+          <Col> PL: <strong><CurrencyFormat value={pl.pl} displayType={'text'} thousandSeparator={true} prefix={'R$ '}/></strong></Col>
+          <Col>D-1: {yesterday.map((e) => (<strong><CurrencyFormat value={e.balance} displayType={'text'} thousandSeparator={true} prefix={'R$ '}/></strong>))}</Col>
+          <Col>D0: {dtoday.map((e) => (<strong><CurrencyFormat value={e.balance} displayType={'text'} thousandSeparator={true} prefix={'R$ '}/></strong>))} </Col>
+          <Col>D1: {tomorrow.map((e) => (<strong><CurrencyFormat value={e.balance} displayType={'text'} thousandSeparator={true} prefix={'R$ '}/></strong>))}</Col>
         </Row>
         <Row>
           <Table columns={columns} data={security} /> 
--- src/view/cadTransacaoAtivo.jsx
+++ src/view/cadTransacaoAtivo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect, Component } from 'react'; 
 import { useNavigate, useParams } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container';
@@ -6,19 +6,22 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
+import Select from 'react-select';
 
 /* values to send to the api*/
 const initialValue = {desc_transaction: "", value_transaction: "", date_transaction: "",  fund_id: "", security_id: "", quantity: ""};
 
-/* Link do request the api*/
+const today = new Date();
 
 const TransactionSecurity = () => {
   const [values, setValues] = useState(initialValue);
   const [funds, setFunds] = useState([]);
   const [security, setSecuritys] = useState([]);
+  const [price, setPrice] = useState([]);
   const navigate = useNavigate();
   const {id} = useParams() || null;
   const buttonText = id ? "Atualizar" : "Cadastrar"; 
+  let t;
 
   useEffect(() => {
     if (id) {
@@ -39,13 +42,35 @@ const TransactionSecurity = () => {
         .then((resp) => { setSecuritys(resp.data); })
         .catch((err) => { console.log(err); });
     }, []);
- console.log(values);
-  
+let aux;
+
   const onChangeEvent = (e) => {
     const {name, value} = e.target
     setValues({...values, [name]:value});
-  }
+  } 
+  
+  const loadSecurityValue = () => {
+    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ (today.getDate() - 1);
+    axios
+      .get(`http://localhost:3001/precoAtivos/${values.security_id}`)
+        .then((resp) => {
+          setPrice(resp.data);
+        }) 
+        .catch((err) => { console.log(err); });
+    let aux = price[0];
+   /* const newValues = { 
+      desc_transaction: values.desc_transaction, 
+      value_transaction: aux.closing_price, 
+      date_transaction: values.date_transaction,  
+      fund_id: values.fund_id, 
+      security_id: values.security_id, 
+      quantity: values.quantity
+    }; */
 
+    setValues(values, {value_transaction: aux.closing_price});  
+   
+  }
+  
   const onSubmit = (e) => {
     e.preventDefault();
     const method = id ? "put" : "post";
@@ -57,22 +82,21 @@ const TransactionSecurity = () => {
         fund_id: values.fund_id
       }
       axios.post(`http://localhost:3001/cash_transactions.json`, {cash_transaction: cash_values}) 
-        .then((response) => {
-          navigate("/transacaoCaixa")
-        })
+        .then()
         .catch((err) => {
           console.log(err);
         });
-    }     
-    axios[method](`http://localhost:3001/securitys_transactions${id ? `/${id}` : ''}.json`, {security_transactions: values}) 
+    }
+    loadSecurityValue();     
+    /*axios[method](`http://localhost:3001/securitys_transactions${id ? `/${id}` : ''}.json`, {security_transactions: values}) 
       .then((response) => {
         navigate("/transacaoAtivos") 
       })
       .catch((err) => {
         console.log(err);
-      });
+      });*/
   }
-
+  console.log(values);
   return (
   <Container fluid="sm" className="mt-4">
       <Form>
@@ -80,12 +104,6 @@ const TransactionSecurity = () => {
             <Form.Group className="mb-3" controlId="formInputDesc">
             <Form.Label>Descrição sobre a Transação:</Form.Label>
             <Form.Control requires name="desc_transaction" type="text" placeholder="Descrição"  value={values.desc_transaction} onChange={onChangeEvent} />
-          </Form.Group>
-        </Row>
-        <Row>
-            <Form.Group className="mb-3" controlId="formInputDesc">
-            <Form.Label>Valor:</Form.Label>
-            <Form.Control requires name="value_transaction" type="text" placeholder="Descrição" value={values.value_transaction}  onChange={onChangeEvent} />
           </Form.Group>
         </Row>
         <Row>
